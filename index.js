df06761b7f0b04a7b33aa1bd40c76337bc1cde9e
M0nicah <monicawughanga@gmail.com>
2021-12-12 19:35:12 +0300

    add constant values to JS  file

---
+++ index.js
@@ -0,0 +1,4 @@
+//Male akan names
+const maleAkanNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+//female akan names
+const femaleAkanNames = ["Akosua", "Adwoa","Abenaa", "Akua", "Yaa", "Afua", "Ama"];
