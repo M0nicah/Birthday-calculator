d4bcddd7edd7e7a16b391f3f30e780e8d0fa2e3a
M0nicah <monicawughanga@gmail.com>
2021-12-12 20:27:53 +0300

    add JS code to collect date

--- index.js
+++ index.js
@@ -1,4 +1,29 @@
 //Male akan names
-const maleAkanNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+const maleAkanNames = [
+  "Kwasi",
+  "Kwadwo",
+  "Kwabena",
+  "Kwaku",
+  "Yaw",
+  "Kofi",
+  "Kwame",
+];
 //female akan names
-const femaleAkanNames = ["Akosua", "Adwoa","Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+const femaleAkanNames = [
+  "Akosua",
+  "Adwoa",
+  "Abenaa",
+  "Akua",
+  "Yaa",
+  "Afua",
+  "Ama",
+];
+
+// submit button click event listener-
+document.getElementById("button").addEventListener(onclick, () => {
+  const date = document.getElementById("date").value;
+  const dateParts = date.split("-");
+  const year = dateParts[0];
+  const month = dateParts[1]; 
+  const date = dateParts[2];
+});
